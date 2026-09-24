f16781102a924313a7b69ae7462b0f9b295e63f7
! - Soren <jgarland13191@gmail.com>
2026-09-23 20:53:38 -0400

    Increase approx line length from 126 to 136

--- api-lib/cards/about.ts
+++ api-lib/cards/about.ts
@@ -43,7 +43,7 @@ function aboutSvg(usernameClean: string) {
     const words = BIO.split(/\s+/);
     const lines: string[] = [];
     let cur = '';
-    const approx = 126;
+    const approx = 136;
     for (const w of words) {
       const cand = cur ? `${cur} ${w}` : w;
       if (cand.length > approx && cur) { lines.push(cur); cur = w; }
